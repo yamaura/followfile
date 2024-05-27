c15feb6daa0b4d274a87f32a6ef8dfbdb0c2faee
Yuki Yamaura <yamaura@pezy.co.jp>
2024-05-27 15:18:41 +0900

    Add follow option

--- examples/tail.rs
+++ examples/tail.rs
@@ -1,14 +1,29 @@
+use clap::Parser;
+use followfile::FollowOptions;
 use std::io::Write;
-use followfile::File;
-    use tokio::io::AsyncReadExt;
+use tokio::io::AsyncReadExt;
+
+#[derive(Parser)]
+struct Args {
+    #[arg(short, long)]
+    follow: bool,
+
+    path: std::path::PathBuf,
+}
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() -> std::io::Result<()> {
-    let input = tokio::fs::File::open(std::env::args().nth(1).unwrap()).await?;
-    let mut input = File::from_reader(input);
+    let args = Args::parse();
+    let input = tokio::fs::File::open(&args.path).await?;
+    let mut input = FollowOptions::default()
+        .stop_eof(!args.follow)
+        .from_reader(input);
 
     let mut buf = vec![0; 1024];
     while let Ok(n) = input.read(&mut buf).await {
+        if n == 0 {
+            break;
+        }
         std::io::stdout().write_all(&buf[..n])?;
     }
     Ok(())
--- src/lib.rs
+++ src/lib.rs
@@ -3,25 +3,51 @@ use std::io::{self};
 use core::task::{Context, Poll};
 use std::pin::Pin;
 
-pub struct File<R, W> {
+pub struct FollowFile<R, W> {
     inner: R,
+    option: FollowOptions,
     watcher: Pin<Box<W>>,
 }
 
 #[cfg(feature = "tokio")]
-impl<R> File<R, Watcher> {
+impl<R> FollowFile<R, Watcher> {
     pub fn from_reader(file: R) -> Self {
         Self {
             inner: file,
+            option: FollowOptions::default(),
             watcher: Box::pin(Watcher::default()),
         }
+    }
+}
+
+#[cfg(feature = "tokio")]
+impl FollowOptions {
+    pub fn from_reader<R>(self, file: R) -> FollowFile<R, Watcher> {
+        FollowFile {
+            inner: file,
+            option: self,
+            watcher: Box::pin(Watcher::default()),
+        }
+    }
+}
+
+#[derive(Default)]
+pub struct FollowOptions {
+    /// Stop reading at EOF (DO NOT follow the file)
+    stop_eof: bool,
+}
+
+impl FollowOptions {
+    pub fn stop_eof(mut self, stop_eof: bool) -> Self {
+        self.stop_eof = stop_eof;
+        self
     }
 }
 
 pub enum Watcher {
     BusyLoop,
     #[cfg(feature = "tokio")]
-#[cfg_attr(docsrs, doc(cfg(feature = "tokio")))]
+    #[cfg_attr(docsrs, doc(cfg(feature = "tokio")))]
     Polling(tokio::time::Interval),
 }
 
@@ -57,7 +83,7 @@ impl core::future::Future for Watcher {
 
 #[cfg(feature = "tokio")]
 #[cfg_attr(docsrs, doc(cfg(feature = "tokio")))]
-impl<R, W> tokio::io::AsyncRead for File<R, W>
+impl<R, W> tokio::io::AsyncRead for FollowFile<R, W>
 where
     R: tokio::io::AsyncRead + std::marker::Unpin,
     W: core::future::Future<Output = ()>,
@@ -68,8 +94,8 @@ where
         buf: &mut tokio::io::ReadBuf<'_>,
     ) -> Poll<io::Result<()>> {
         match Pin::new(&mut self.inner).poll_read(cx, buf) {
-            Poll::Ready(Ok(_)) => {
-                if buf.filled().is_empty() {
+            Poll::Ready(Ok(())) => {
+                if !self.option.stop_eof && buf.filled().is_empty() {
                     // be careful this leads to busy loop
                     loop {
                         match self.watcher.as_mut().poll(cx) {
